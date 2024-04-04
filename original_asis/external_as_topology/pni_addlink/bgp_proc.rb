6598421003aabf7264f6cd402ff5539b46f7ae3a
t-tkgh <you@example.com>
2024-04-05 02:12:33 +0900

    janog53 demo

--- original_asis/external_as_topology/pni_addlink/bgp_proc.rb
+++ original_asis/external_as_topology/pni_addlink/bgp_proc.rb
@@ -16,24 +16,14 @@ def register_bgp_proc(nws)
         term_point 'peer_172.16.0.6' do
           support %w[layer3 AS65550-1  Ethernet1]
           attribute(
-            flags: %w[ext-bgp-speaker-preferred],
             local_as: 65_550,
             local_ip: '172.16.0.5',
             remote_as: 65_518,
             remote_ip: '172.16.0.6'
           )
         end
-        term_point 'peer_172.16.1.18' do
-          support %w[layer3 AS65550-1  Ethernet2]
-          attribute(
-            local_as: 65_550,
-            local_ip: '172.16.1.17',
-            remote_as: 65_518,
-            remote_ip: '172.16.1.18'
-          )
-        end
         term_point 'peer_169.254.0.2' do
-          support %w[layer3 AS65550-1  Ethernet3]
+          support %w[layer3 AS65550-1  Ethernet2]
           attribute(
             local_as: 65_550,
             local_ip: '169.254.0.1',
@@ -41,8 +31,18 @@ def register_bgp_proc(nws)
             remote_ip: '169.254.0.2'
           )
         end
+        term_point 'peer_169.254.0.6' do
+          support %w[layer3 AS65550-1  Ethernet3]
+          attribute(
+            local_as: 65_550,
+            local_ip: '169.254.0.5',
+            remote_as: 65_550,
+            remote_ip: '169.254.0.6'
+          )
+        end
       end
       bdlink %w[AS65550-1 peer_169.254.0.2 AS65550-2 peer_169.254.0.1]
+      bdlink %w[AS65550-1 peer_169.254.0.6 AS65550ADD peer_169.254.0.5]
       node 'AS65550-2' do
         support %w[layer3 AS65550-2]
         attribute(
@@ -67,12 +67,47 @@ def register_bgp_proc(nws)
             remote_ip: '169.254.0.1'
           )
         end
+        term_point 'peer_169.254.0.10' do
+          support %w[layer3 AS65550-2  Ethernet3]
+          attribute(
+            local_as: 65_550,
+            local_ip: '169.254.0.9',
+            remote_as: 65_550,
+            remote_ip: '169.254.0.10'
+          )
+        end
+      end
+      bdlink %w[AS65550-2 peer_169.254.0.10 AS65550ADD peer_169.254.0.9]
+      node 'AS65550ADD' do
+        support %w[layer3 AS65550ADD]
+        attribute(
+          flags: %w[ext-bgp-speaker],
+          router_id: '0.0.0.3' # TODO
+        )
+        term_point 'peer_169.254.0.5' do
+          support %w[layer3 AS65550ADD  Ethernet2]
+          attribute(
+            local_as: 65_550,
+            local_ip: '169.254.0.6',
+            remote_as: 65_550,
+            remote_ip: '169.254.0.5'
+          )
+        end
+        term_point 'peer_169.254.0.9' do
+          support %w[layer3 AS65550ADD  Ethernet3]
+          attribute(
+            local_as: 65_550,
+            local_ip: '169.254.0.10',
+            remote_as: 65_550,
+            remote_ip: '169.254.0.9'
+          )
+        end
       end
       node 'AS65520-1' do
         support %w[layer3 AS65520-1]
         attribute(
           flags: %w[ext-bgp-speaker],
-          router_id: '0.0.0.3' # TODO
+          router_id: '0.0.0.4' # TODO
         )
         term_point 'peer_192.168.0.9' do
           support %w[layer3 AS65520-1  Ethernet1]
@@ -83,53 +118,8 @@ def register_bgp_proc(nws)
             remote_ip: '192.168.0.9'
           )
         end
-        term_point 'peer_169.254.0.6' do
-          support %w[layer3 AS65520-1  Ethernet2]
-          attribute(
-            local_as: 65_520,
-            local_ip: '169.254.0.5',
-            remote_as: 65_520,
-            remote_ip: '169.254.0.6'
-          )
-        end
-        term_point 'peer_169.254.0.10' do
-          support %w[layer3 AS65520-1  Ethernet3]
-          attribute(
-            local_as: 65_520,
-            local_ip: '169.254.0.9',
-            remote_as: 65_520,
-            remote_ip: '169.254.0.10'
-          )
-        end
-      end
-      bdlink %w[AS65520-1 peer_169.254.0.6 AS65520-2 peer_169.254.0.5]
-      bdlink %w[AS65520-1 peer_169.254.0.10 AS65520-3 peer_169.254.0.9]
-      node 'AS65520-2' do
-        support %w[layer3 AS65520-2]
-        attribute(
-          flags: %w[ext-bgp-speaker],
-          router_id: '0.0.0.4' # TODO
-        )
-        term_point 'peer_192.168.0.13' do
-          support %w[layer3 AS65520-2  Ethernet1]
-          attribute(
-            local_as: 65_520,
-            local_ip: '192.168.0.14',
-            remote_as: 65_518,
-            remote_ip: '192.168.0.13'
-          )
-        end
-        term_point 'peer_169.254.0.5' do
-          support %w[layer3 AS65520-2  Ethernet2]
-          attribute(
-            local_as: 65_520,
-            local_ip: '169.254.0.6',
-            remote_as: 65_520,
-            remote_ip: '169.254.0.5'
-          )
-        end
         term_point 'peer_169.254.0.14' do
-          support %w[layer3 AS65520-2  Ethernet3]
+          support %w[layer3 AS65520-1  Ethernet2]
           attribute(
             local_as: 65_520,
             local_ip: '169.254.0.13',
@@ -138,7 +128,7 @@ def register_bgp_proc(nws)
           )
         end
       end
-      bdlink %w[AS65520-2 peer_169.254.0.14 AS65520-3 peer_169.254.0.13]
+      bdlink %w[AS65520-1 peer_169.254.0.14 AS65520-3 peer_169.254.0.13]
       node 'AS65520-3' do
         support %w[layer3 AS65520-3]
         attribute(
@@ -154,17 +144,8 @@ def register_bgp_proc(nws)
             remote_ip: '192.168.0.17'
           )
         end
-        term_point 'peer_169.254.0.9' do
-          support %w[layer3 AS65520-3  Ethernet2]
-          attribute(
-            local_as: 65_520,
-            local_ip: '169.254.0.10',
-            remote_as: 65_520,
-            remote_ip: '169.254.0.9'
-          )
-        end
         term_point 'peer_169.254.0.13' do
-          support %w[layer3 AS65520-3  Ethernet3]
+          support %w[layer3 AS65520-3  Ethernet2]
           attribute(
             local_as: 65_520,
             local_ip: '169.254.0.14',
